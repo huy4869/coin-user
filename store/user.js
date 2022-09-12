0cfa30b57b12f8c1967fe9779e823bd2b950e60a
nvkhai <khainv@amaisoft.com>
2022-09-12 13:16:02 +0700

    up receive modal

---
+++ store/user.js
@@ -0,0 +1,25 @@
+import { handleApi } from '@/utils/handleApi'
+
+export const state = () => ({
+  roles: [],
+  permissions: [],
+  openModalLoginState: true,
+  resetCartState: true
+})
+
+export const mutations = {
+  setRoles(state, roles) {
+    state.roles = roles
+  },
+  setPermissions(state, permissions) {
+    state.permissions = permissions
+  }
+}
+
+export const actions = {
+  getWallet(context) {
+    return new Promise((resolve, reject) => {
+      handleApi(resolve, reject, this.$axios.get('/wallets/system'), context)
+    })
+  }
+}
