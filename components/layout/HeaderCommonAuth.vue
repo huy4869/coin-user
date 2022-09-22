62fc99ec70b9c937751bf5ef0def7c601fc9536a
nvkhai <khainv@amaisoft.com>
2022-09-22 17:10:13 +0700

    update feedback

--- components/layout/HeaderCommonAuth.vue
+++ components/layout/HeaderCommonAuth.vue
@@ -6,43 +6,9 @@
           <img src="~/assets/images/logo_header.png" alt="" class="logo_header" @click="redirect('/')">
         </div>
         <div class="header_right">
-          <el-dropdown class="cursor-pointer d-flex lang_div" trigger="click" placement="bottom-start">
-            <span class="img_lang">
-              <img class="image-language" :src="languageActive.icon" alt="">
-              <img class="image-dropdown" src="~/assets/images/icons/arrow_down.svg" alt="">
-            </span>
-            <template #dropdown>
-              <el-dropdown-menu class="dropdown-language">
-                <el-dropdown-item v-for="(language, index) in listLanguage" :key="index" :command="index">
-                  <div class="select-language d-flex" @click="changeLanguage(language)">
-                    <img :src="language.icon" alt="">
-                    <div class="language-name pd-l-10">{{ language.name }}</div>
-                  </div>
-                </el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
         </div>
         <!--  -->
         <div class="header-main-menu-mobile">
-          <div class="header_right lang_mobile_div">
-            <el-dropdown class="cursor-pointer d-flex lang_div" trigger="click" placement="bottom-start">
-            <span class="img_lang">
-              <img class="image-language" :src="languageActive.icon" alt="">
-              <img class="image-dropdown" src="~/assets/images/icons/arrow_down.svg" alt="">
-            </span>
-              <template #dropdown>
-                <el-dropdown-menu class="dropdown-language">
-                  <el-dropdown-item v-for="(language, index) in listLanguage" :key="index" :command="index">
-                    <div class="select-language d-flex" @click="changeLanguage(language)">
-                      <img :src="language.icon" alt="">
-                      <div class="language-name pd-l-10">{{ language.name }}</div>
-                    </div>
-                  </el-dropdown-item>
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown>
-          </div>
           <label for="nav-mobile-input">
             <div class="menu-mobile pointer">
               <img src="@/assets/images/icons/menu.svg" alt="">
--- components/modals/modal-deposit.vue
+++ components/modals/modal-deposit.vue
@@ -8,6 +8,7 @@
         <li><h3 class="sub-title">{{ $t('deposit.direct1') }}</h3></li>
         <li><h3 class="sub-title">{{ $t('deposit.direct2') }}</h3></li>
         <li><h3 class="sub-title">{{ $t('deposit.direct3') }}</h3></li>
+        <li><h3 class="sub-title">{{ $t('deposit.direct4') }}</h3></li>
       </ul>
       <el-form
         autocomplete="off"
@@ -43,6 +44,36 @@
               @click="copy(memo)">
           </el-input>
         </el-form-item>
+
+        <label for="amount" class="label_node_div">
+          <span class="label_node">{{ $t('deposit.activate') }}</span>
+          <span class="value_node">{{ totalPrice }} CHZ</span>
+        </label>
+        <el-form-item
+          id="amount"
+          class="email-login amount_input"
+        >
+          <el-input
+            name="amount"
+            type="number"
+            :value="amount"
+            tabindex="1"
+            maxlength="3"
+            :max="this.amount_max"
+            :min="1"
+            oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+            pattern="[0-9]*" inputmode="numeric"
+          >
+            <div :class="['btn_amount', {'btn_disable' : amount <= 1}]"
+                 slot="prepend" @click="changeAmount('-')">
+              <el-button :disabled="amount <= 1" class="btn_amount_el">-</el-button>
+            </div>
+            <div :class="['btn_amount', {'btn_disable' : amount >= this.amount_max}]"
+                 slot="append" @click="changeAmount('+')">
+              <el-button :disabled="amount >= this.amount_max" class="btn_amount_el">+</el-button>
+            </div>
+          </el-input>
+        </el-form-item>
         <div class="common-button">
           <el-button
             @click="close"
@@ -73,12 +104,19 @@ export default {
   },
   data() {
     return {
+      amount: 1,
+      amount_max: 100000000,
+      total_price: 150,
       valid: false,
       loading: false,
       fullscreenLoading: false
     }
   },
-  computed: {},
+  computed: {
+    totalPrice() {
+      return Number(this.amount) * this.total_price
+    }
+  },
   methods: {
     close() {
       this.$emit('close')
@@ -94,7 +132,105 @@ export default {
         show: true,
         text: this.$t('noti.copy_success')
       })
+    },
+    changeAmount(type) {
+      if (type === '+' && this.amount < this.amount_max) {
+        this.amount++
+      } else if (type === '-' && this.amount > 1) {
+        this.amount--
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "assets/scss/variables";
+
+.price_div {
+  width: 100%;
+  box-sizing: border-box;
+  //border: 1px solid $color-border-sidebar;
+  border: 1px solid #DCDFE6;
+  border-radius: 6px;
+  padding: 11px 12px;
+  display: flex;
+  justify-content: left;
+  gap: 50px;
+  align-items: center;
+
+  .logo_price {
+    width: fit-content;
+    display: flex;
+    justify-content: space-between;
+    gap: 17px;
+
+    .price_text {
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 29px;
+      text-align: center;
+      color: $color-red-primary;
+    }
+  }
+}
+
+.info_div {
+  padding: 17px 12px;
+  display: grid;
+  grid-template-columns: auto;
+  gap: 30px;
+
+  .item_div {
+    display: flex;
+    justify-content: left;
+    gap: 18px;
+    align-items: center;
+
+    img {
+      border-radius: unset;
+    }
+  }
+
+  .des_info {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 25px;
+
+    color: $color-text-6;
+    max-width: 100%;
+    word-break: break-word;
+
+  }
+}
+
+.amount_input {
+  padding-top: 10px;
+  padding-bottom: 15px;
+}
+
+.btn_amount_el {
+  box-shadow: unset;
+  background: unset;
+}
+
+.label_node_div {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  .label_node {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+  }
+
+  .value_node {
+    font-weight: 800;
+    font-size: 20px;
+    line-height: 20px;
+    color: $color-red-primary;
+  }
+}
+</style>
